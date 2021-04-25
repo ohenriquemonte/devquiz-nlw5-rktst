9fbe276fff3410a44199c40322f75d80e75646de
Henrique Montenegro <henriquemontenegro@MacBook-Pro-de-Henrique.local>
2021-04-25 17:15:59 -0300

    Aula 02 - Pagina de Pergunta e Cards de Respostas

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,13 +1,16 @@
+import 'package:DevQuiz/challenge/challenge_page.dart';
 import 'package:DevQuiz/home/home_page.dart';
 import 'package:DevQuiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
+  AppWidget({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: HomePage(),
+      home: ChallengePage(),
     );
   }
 }
