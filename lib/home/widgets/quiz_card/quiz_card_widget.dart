f790a6da2e4e1585f786b193def14a5eee488895
Henrique Montenegro <henriquemontenegro@MacBook-Pro-de-Henrique.local>
2021-04-24 21:31:38 -0300

    Componente QuizCard da HomePage

---
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:DevQuiz/core/app_colors.dart';
+import 'package:DevQuiz/core/core.dart';
+
+class QuizCardWidget extends StatelessWidget {
+  QuizCardWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        border: Border.fromBorderSide(BorderSide(color: AppColors.white)),
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: 40,
+            height: 40,
+            child: Image.asset(AppImages.blocks),
+          ),
+          SizedBox(height: 20),
+          Text('Gerenciamento de Estado', style: AppTextStyles.heading15),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Text('3 de 10', style: AppTextStyles.body11),
+              ),
+              Expanded(
+                flex: 2,
+                child: LinearProgressIndicator(
+                  value: 0.3,
+                  backgroundColor: AppColors.chartSecondary,
+                  valueColor:
+                      AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
